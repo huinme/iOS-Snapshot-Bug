390f0ba172c97261fa97921ba1666fdaa1b5474c
kshuin <koichi.skt@gmail.com>
2014-10-15 13:56:21 +0900

    Initial Commit

---
+++ iOS8-Rendering-Bug/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOS8-Rendering-Bug
+//
+//  Created by huin on 2014/10/15.
+//  Copyright (c) 2014年 yourcompany.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS8-Rendering-Bug/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOS8-Rendering-Bug
+//
+//  Created by huin on 2014/10/15.
+//  Copyright (c) 2014年 yourcompany.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
